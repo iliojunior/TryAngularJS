b1e17508aac0f847f0b4ac4aa0e897c3edae206e
SouzaEduardoAC <souzaeduardoac@gmail.com>
2017-03-18 17:59:51 -0300

    module 1

---
+++ app.js
@@ -0,0 +1,26 @@
+/**
+ * Created by souzaeduardoac on 18/03/17.
+ */
+(function(){
+    var app = angular.module('store',[ ]);
+
+    app.controller('StoreController', function(){
+        this.products = gems;
+    });
+
+    var gems = [
+        {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: '. . .',
+        canPurchase: true,
+        soldOut: true,
+        },
+        {
+            name: 'Pentagonal Gem',
+            price: 5.95,
+            description: '. . .',
+            canPurchase: false,
+        }
+    ];
+})();
