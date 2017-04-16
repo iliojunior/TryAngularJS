87bc31783828abf694a32db95412d7304328449d
SouzaEduardoAC <souzaeduardoac@gmail.com>
2017-04-16 15:00:59 -0300

    termino do curso

--- products.js
+++ products.js
@@ -1,60 +1,42 @@
-/**
- * Created by souzaeduardoac on 16/04/17.
- */
-(function() {
-    var app = angular.module('store-directives',[ ]);
-    app.directive("productDescription", function() {
-        return {
-            restrict: 'E',
-            templateUrl: "product-description.html"
-        };
-    });
+var app = angular.module('store-directives', []);
 
-    app.directive("productReviews", function() {
-        return {
-            restrict: 'E',
-            templateUrl: "product-reviews.html"
-        };
-    });
+app.directive("descriptions", function() {
+  return {
+    restrict: "E",
+    templateUrl: "descriptions.html"
+  };
+});
 
-    app.directive("productSpecs", function() {
-        return {
-            restrict:"A",
-            templateUrl: "product-specs.html"
-        };
-    });
+app.directive("reviews", function() {
+  return {
+    restrict: "E",
+    templateUrl: "reviews.html"
+  };
+});
 
-    app.directive("productTabs", function() {
-        return {
-            restrict: "E",
-            templateUrl: "product-tabs.html",
-            controller: function() {
-                this.tab = 1;
+app.directive("specs", function() {
+  return {
+    restrict: "E",
+    templateUrl: "specs.html"
+  };
+});
 
-                this.isSet = function(checkTab) {
-                    return this.tab === checkTab;
-                };
+app.directive("productTabs", function() {
+  return {
+    restrict: "E",
 
-                this.setTab = function(activeTab) {
-                    this.tab = activeTab;
-                };
-            },
-            controllerAs: "tab"
-        };
-    });
+    templateUrl: "product-tabs.html",
+    controller: function() {
+      this.tab = 1;
 
-    app.directive("productGallery", function() {
-        return {
-            restrict: "E",
-            templateUrl: "product-gallery.html",
-            controller: function() {
-                this.current = 0;
-                this.setCurrent = function(imageNumber){
-                    this.current = imageNumber || 0;
-                };
-            },
-            controllerAs: "gallery"
-        };
-    });
+      this.isSet = function(checkTab) {
+        return this.tab === checkTab;
+      };
 
-})();
+      this.setTab = function(activeTab) {
+        this.tab = activeTab;
+      };
+    },
+    controllerAs: "tab"
+  };
+});
